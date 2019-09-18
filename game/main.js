dbd5c97306fccf7c505c301d2ceee26fe12672a6
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-09-17 23:08:54 -0400

    finished project

--- game/main.js
+++ game/main.js
@@ -9,17 +9,14 @@ let equationType = ['+', '-', '*', '/'];
 let correctAnswer;
 let points = 0;
 let timer = 30;
+let interval;
 
 function results(eq, a, b) {
     switch(eq) {
         case '+': return a + b;
-            break;
         case '-': return a - b;
-            break;
         case '*': return a * b;
-            break;
         case '/': return a / b;
-            break;
     }
 }
 
@@ -28,23 +25,26 @@ function randomNumber() {
     return Math.floor(Math.random() * (100) + 1);
 }
 
-/* 
-* Main question method which gathers a random equation type and random numbers for those equations
-*/
+//Main question method which gathers a random equation type and random numbers for those equations
 function question() {
-    let eqType = equationType[Math.floor(Math.random() * 4)]; // randomly pick equation type
+    // randomly pick equation type
+    let eqType = equationType[Math.floor(Math.random() * 4)]; 
     let a = randomNumber();
     let b = randomNumber();
     let equation = results(eqType, a, b);
+    (!Number.isInteger(equation)) ? equation = equation.toFixed(2) : '';
     correctAnswer = equation;
     document.querySelector("#question").innerHTML =  "What is " + a + eqType + b + "?";
-    let randomCorrectAnswer = Math.floor(Math.random() * 3);
+    // randomly pick which option gets the correct answer
+    let randomCorrectAnswer = Math.floor(Math.random() * 3); 
     for(let i = 0; i < document.getElementsByClassName('answer').length; i++){
         let c = randomNumber();
         let d = randomNumber();
+        let randomAnswer = results(eqType, c, d);
+        (!Number.isInteger(randomAnswer)) ? randomAnswer = randomAnswer.toFixed(2) : '' ;
         (i == randomCorrectAnswer) ? 
             document.getElementsByClassName('answer')[i].innerText = equation : 
-            document.getElementsByClassName('answer')[i].innerText = results(eqType, c, d); // send same equation type but different numbers
+            document.getElementsByClassName('answer')[i].innerText = randomAnswer; // send same equation type but different numbers
     }
 }
 
@@ -61,25 +61,18 @@ for(let i = 0; i < 3; i++) {
     });
 }
 
-document.querySelector('#resetGame').addEventListener("click", function(e){
-    document.querySelector('#resetGame').innerText = "Reset Game";
-    startGame();
-    e.preventDefault();
-});
-
-function startGame(){
-    let interval;
-    window.clearInterval(interval);
-    interval = undefined;
+//Initializes game and resets timer to 30 seconds. Timer turns red when 5 secons remaining.
+function startGame(){    
     points = 0;
     timer = 30;
-    document.querySelector('#timer').innerText = timer;
+    document.querySelector('#timer').innerText = timer + " seconds";
     question();
     interval = window.setInterval(function() {
             timer--;
-            document.querySelector('#timer').innerText = timer;
+            document.querySelector('#timer').innerText = timer + " seconds";
             if(timer == 0) {
                 window.clearInterval(interval);
+                document.querySelector('#resetGame').innerText = "Start Game";
                 for(let i = 0; i < 3; i++) {
                     document.getElementsByClassName('answer')[i].disabled = true;
                     document.querySelector('#timer').innerText = "Time's Up!";
@@ -89,3 +82,14 @@ function startGame(){
             }
         }, 1000);
 }
+
+document.querySelector('#resetGame').addEventListener("click", function(e){
+    if(document.querySelector('#resetGame').innerText == "Start Game") {
+        startGame();
+        document.querySelector('#resetGame').innerText = "Stop Game";
+    } else {
+        window.clearInterval(interval);
+        document.querySelector('#resetGame').innerText = "Start Game";
+    }
+    e.preventDefault();
+});
