ad5081d1cc2e1a48eb3096035544a91c9a708290
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-11-15 00:06:42 -0500

    p3 Shell complete

---
+++ p3/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Vue Package Manager</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active" v-for='link in links' :key='link'>
+            <router-link class="nav-link" exact :to='{name: link.context}'>{{ link.name }}</router-link>
+            <!--<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>-->
+          </li>
+        </ul>
+      </div>
+    </nav>
+   
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data: function () {
+      return {
+          links: [{name: 'Home', context: 'home'}, {name: 'All Packages', context: 'allPackages'}, {name: 'My Packages', context: 'myPackages'}]
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+</style>
---
+++ p3/src/components/MyPackages.vue
@@ -0,0 +1,13 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+    export default {
+        name: "MyPackages"
+    }
+</script>
+
+<style scoped>
+
+</style>
