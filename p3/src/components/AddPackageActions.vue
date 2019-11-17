14a639df0085d560f9620df054a19096f59cfe70
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-11-17 01:08:35 -0500

    add/remove package functionality added

--- p3/src/components/AddPackageActions.vue
+++ p3/src/components/AddPackageActions.vue
@@ -1,8 +1,7 @@
 <template>
 	<div>
-		<a href="#" class="card-link" >View Package Details</a>
-		<router-link class="card-link" exact :to='{name: "packageDetail", params: {id: packageId}}'>Blah</router-link>
-		<a href="#" class="card-link" :package-id="packageId">Add Package to List</a>
+		<router-link class="card-link" exact :to='{name: "packageDetail", params: {id: packageId}}'>View Package Details</router-link>
+		<a href="#" class="card-link" :package-id="packageId" @click.prevent="addToMyPackages">{{ addToPackagesText }}</a>
 	</div>
 </template>
 
@@ -11,8 +10,39 @@
         name: "AddPackageActions",
 		props: ['packageId'],
 		data: function () {
-			return {}
-        }
+			return {
+				addToPackagesText: 'Add Package'
+			}
+        },
+		methods: {
+            addToMyPackages: function () {
+                let myLocalPackages = [];
+                if(localStorage.getItem('myPackages')){
+                    myLocalPackages = JSON.parse(localStorage.getItem('myPackages'));
+                    if(!myLocalPackages.includes(this.packageId)) {
+                        myLocalPackages.push(this.packageId);
+                        this.addToPackagesText = 'Remove Package';
+                    } else {
+                        // Remove package
+                        this.addToPackagesText = 'Add Package';
+						myLocalPackages = myLocalPackages.filter( item => {
+							return item != this.packageId;
+						});
+                    }
+                } else {
+                    myLocalPackages.push(this.packageId);
+                }
+                localStorage.setItem('myPackages', JSON.stringify(myLocalPackages));
+            }
+		},
+		mounted() {
+            if(localStorage.getItem('myPackages')){
+                let myLocalPackages = JSON.parse(localStorage.getItem('myPackages'));
+                if(myLocalPackages.includes(this.packageId)){
+                    this.addToPackagesText = 'Remove Package';
+                }
+            }
+		}
     }
 </script>
 
--- p3/src/components/AllPackages.vue
+++ p3/src/components/AllPackages.vue
@@ -1,27 +1,40 @@
 <template>
 	<div>
+		<input class="form-control" type="text" v-model="search" @keyup="filterPackages" placeholder="Search Packages"/>
 		<div class="card text-left" v-for='pack in allPackages' :key="pack.id">
 			<div class="card-body">
 				<h5 class="card-title">{{ pack.name }}</h5>
 				<h6 class="card-subtitle mb-2 text-muted">{{ pack.category }}</h6>
 				<p class="card-text">{{ pack.description }}</p>
-				<add-package></add-package>
+				<add-package-actions :package-id="pack.id"></add-package-actions>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-	import AddPackage from './AddPackageActions.vue';
+	import AddPackageActions from './AddPackageActions.vue';
     const axios = require('axios');
     export default {
         name: "AllPackages",
-		components: {AddPackage},
+		components: {AddPackageActions},
         data: function(){
             return {
-                allPackages: null
+                allPackages: null,
+				filteredPackages: null,
+				search: null
             }
         },
+		methods: {
+            filterPackages: function () {
+				this.allPackages = this.filteredPackages.filter( item => {
+					let searchItem = this.search.toLowerCase();
+					return  (item.name.toLowerCase().indexOf(searchItem) > -1) ||
+						(item.category.toLowerCase().indexOf(searchItem) > -1) ||
+						(item.description.toLowerCase().indexOf(searchItem) > -1);
+				});
+            }
+		},
         mounted() {
             this.packages = axios
                 .get(
@@ -29,11 +42,14 @@
                 )
                 .then(response => {
                     this.allPackages = response.data;
+                    this.filteredPackages = response.data;
                 });
         }
     }
 </script>
 
 <style scoped>
-
+	.form-control {
+		margin: 30px 0;
+	}
 </style>
