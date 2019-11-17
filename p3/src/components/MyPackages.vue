a948b39222df699f4ef9d337bace6fd6616f35c3
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-11-17 03:07:24 -0500

    myPackages component working with localStorage

--- p3/src/components/MyPackages.vue
+++ p3/src/components/MyPackages.vue
@@ -1,10 +1,47 @@
 <template>
-	<div></div>
+	<div>
+		<div v-if="myPackages.length > 0">
+		<div v-for="pack in myPackages" :key="pack.id">
+			<package-card :pack="pack"></package-card>
+		</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	import PackageCard from './PackageCard.vue';
+    const axios = require('axios');
     export default {
-        name: "MyPackages"
+        name: "MyPackages",
+		components: {PackageCard},
+		data: function(){
+            return {
+                myPackages: []
+            }
+		},
+        mounted() {
+            if(localStorage.getItem('myPackages')) {
+                this.myPackages = [];
+                let packageIds = JSON.parse(localStorage.getItem('myPackages'));
+                packageIds.map( item => {
+                    console.log(item);
+                    axios
+                        .get(
+                            'https://my-json-server.typicode.com/naztronaut/e28api/packages/' + item
+                        )
+                        .then(response => {
+                            this.myPackages.push(response.data);
+                        });
+                });
+            } else {
+                this.myPackages = [{
+                    id: 999,
+                    name: "You didn't add any packages, go back to the All Packages page and add some!",
+                    category: "",
+                    description: ""
+                }];
+            }
+        }
     }
 </script>
 
